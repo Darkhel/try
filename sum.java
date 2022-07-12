d1d3c20d001c10d8496cecb8b0383e5a19d60bd9
Darkhel <108645933+Darkhel@users.noreply.github.com>
2022-07-12 19:37:28 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,8 @@
+
+public class Sumofnumbers1 {
+    public static void main(String args[] ) {
+      int n1 = 1 ,n2 = 2 , n3 = 5 , n4 = 4 , n5 = 5 , sum;
+        sum = n1 + n2 + n3 + n4 + n5 ;
+          System.out.println("The sum of numbers is: " +sum);
+   }
+}
